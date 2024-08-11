5f1c379a93733aad50674a2cdc79bcf4298793c7
toanminhbui <toanbui@umich.edu>
2024-08-11 22:00:27 +0700

    pushed fix to escaped apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import InputForm from "@/components/search_form";
 export default function Home() {
   return (
     <main className="flex items-center flex-col">
-      <h1 className="text-4xl font-mono">Toan's Netflix Guide</h1>
+      <h1 className="text-4xl font-mono">Toan&apos;s Netflix Guide</h1>
       <h3 className="text-xl mb-20" style={{ color: '#718096' }}>semantically search a database of over 4000 genres and categories</h3>
       <div className="flex items-center font-mono text-sm lg:flex">
        <InputForm/>
